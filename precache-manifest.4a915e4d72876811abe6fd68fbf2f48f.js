374dbeebb13b69a6d550e6c81b6d3fe04c303de2
cbabey <cbabeyofficial@gmail.com>
2020-04-28 20:44:24 +0530

    Initial dist subtree commit

---
+++ precache-manifest.4a915e4d72876811abe6fd68fbf2f48f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ca7441534e0fa4cc55",
+    "url": "/TestingVueApp/css/app.673fc470.css"
+  },
+  {
+    "revision": "a2ca7441534e0fa4cc55",
+    "url": "/TestingVueApp/js/app.9ccc5c23.js"
+  },
+  {
+    "revision": "e16d36711d07caecdc1e",
+    "url": "/TestingVueApp/js/chunk-52e4de86.e638d5a7.js"
+  },
+  {
+    "revision": "ba7310c7030139d4dc9c",
+    "url": "/TestingVueApp/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "ba7310c7030139d4dc9c",
+    "url": "/TestingVueApp/js/chunk-ebdaf33a.f6275555.js"
+  },
+  {
+    "revision": "543c3d75724d3a026098",
+    "url": "/TestingVueApp/js/chunk-vendors.aa1771e6.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/TestingVueApp/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/TestingVueApp/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/TestingVueApp/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/TestingVueApp/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/TestingVueApp/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TestingVueApp/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TestingVueApp/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TestingVueApp/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TestingVueApp/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TestingVueApp/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "058e01e70465e179ccff9713633a5432",
+    "url": "/TestingVueApp/index.html"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/TestingVueApp/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/TestingVueApp/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/TestingVueApp/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/TestingVueApp/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/TestingVueApp/img/brand/favicon.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/TestingVueApp/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/TestingVueApp/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/TestingVueApp/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/TestingVueApp/img/theme/promo-1.png"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/TestingVueApp/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/TestingVueApp/img/theme/profile.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/TestingVueApp/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/TestingVueApp/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/TestingVueApp/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/TestingVueApp/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/TestingVueApp/img/theme/team-2-800x800.jpg"
+  }
+];
